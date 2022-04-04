8e07bfcfd5df2d6371e7581cd9e51cf61e4a7d35
vad3mon <vadimka2703@gmail.com>
2022-04-04 15:47:07 +0300

    add Toolbar

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,13 @@
     import MainPage from "./MainPage.svelte";
     import SignInPage from "./SignInPage.svelte";
     import SignUpPage from "./SignUpPage.svelte";
+    import PortfolioPage from "./PortfolioPage.svelte";
 </script>
 
-<MainPage/>
-<SignInPage/>
-<SignUpPage/>
-<footer class="p-4 bg-[#464646] mt-12">
-  <p class="text-white text-center text-xl font-thin">© 2022 LEAFSTOCK</p>
-</footer>
+<!--<MainPage/>-->
+<!--<SignInPage/>-->
+<!--<SignUpPage/>-->
+<PortfolioPage/>
+<!--<footer class="p-4 bg-[#464646] mt-12">-->
+<!--  <p class="text-white text-center text-xl font-thin">© 2022 LEAFSTOCK</p>-->
+<!--</footer>-->
---
+++ src/PortfolioPage.svelte
@@ -0,0 +1,25 @@
+<script>
+    import viewIcon from "./assets/images/icon/toolbarIcon/viewIcon.svg";
+    import saveIcon from "./assets/images/icon/toolbarIcon/saveIcon.svg";
+    import mediaIcon from "./assets/images/icon/toolbarIcon/mediaIcon.svg";
+    import templateIcon from "./assets/images/icon/toolbarIcon/templateIcon.svg";
+    import designIcon from "./assets/images/icon/toolbarIcon/designIcon.svg";
+    import adaptiveIcon from "./assets/images/icon/toolbarIcon/adaptiveIcon.svg";
+
+    let logo = 'ls';
+
+</script>
+
+<aside class="grid min-h-screen justify-center content-between gap-15 w-25">
+    <div class="grid gap-15">
+        <a href="#" class='bg-black px-4 py-5 text-white'>{logo}</a>
+        <button class=""><img src="{templateIcon}" alt="Шаблоны"></button>
+        <button class=""><img src="{mediaIcon}" alt="Медия"></button>
+        <button class=""><img src="{designIcon}" alt="Дизайн"></button>
+        <button class=""><img src="{adaptiveIcon}" alt="Адаптив"></button>
+    </div>
+    <div class="grid gap-15">
+        <button><img src="{viewIcon}" alt="Просмотр"></button>
+        <button class="flex justify-center items-center"><img src="{saveIcon}" alt="Сохранить"></button>
+    </div>
+</aside>
