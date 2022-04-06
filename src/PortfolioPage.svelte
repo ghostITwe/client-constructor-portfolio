ed6221fed89c2fdf9c2fa689214a363b4e17c6d1
vad3mon <vadimka2703@gmail.com>
2022-04-06 16:52:56 +0300

    add styles for second toolbar and js function toggle

--- src/PortfolioPage.svelte
+++ src/PortfolioPage.svelte
@@ -13,101 +13,116 @@
     import negativeAdaptiveIcon from "./assets/images/icon/toolbarIcon/negativeAdaptiveIcon.svg";
     import addIcon from "./assets/images/icon/toolbarIcon/addIcon.svg";
     import negativeAddIcon from "./assets/images/icon/toolbarIcon/negativeAddIcon.svg";
+    import leftArrowIcon from "./assets/images/icon/toolbarIcon/leftArrowIcon.svg";
     import rec from "./assets/images/rec.svg";
+    import img from "./assets/123.jpg";
 
-    let logo = 'ls';
+    let logo = 'LS';
+
+    function toggle() {
+        const toolbar = document.getElementById('toolbar');
+        toolbar.classList.toggle('-translate-x-full');
+    }
 
 </script>
 
-<aside class="grid min-h-screen justify-center content-between gap-15 w-25 px-5">
-  <div class="grid gap-15">
-    <a href="#" class="bg-black text-center text-white">{logo}</a>
-    <button class=""><img src="{templateIcon}" alt="Шаблоны"></button>
-    <button class=""><img src="{addIcon}" alt="Добавить"></button>
-    <button class=""><img src="{mediaIcon}" alt="Медия"></button>
-    <button class=""><img src="{designIcon}" alt="Дизайн"></button>
-    <button class=""><img src="{adaptiveIcon}" alt="Адаптив"></button>
-  </div>
-  <div class="grid gap-15 mb-15">
-    <button><img src="{viewIcon}" alt="Просмотр"></button>
-    <button class="flex justify-center items-center"><img src="{saveIcon}" alt="Сохранить"></button>
-  </div>
-</aside>
-<aside class="grid min-h-screen">
-  <section class="grid w-87.5 grid-cols-2 gap-5 content-center">
-    <h3 class="text-white col-span-2 pl-4">Текст</h3>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-  </section>
-  <section class="grid w-87.5 grid-cols-2 gap-5 content-center">
-    <h3 class="text-white col-span-2 pl-4">Текст</h3>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-  </section>
-  <section class="grid w-87.5 grid-cols-2 gap-5 content-center">
-    <h3 class="text-white col-span-2 pl-4">Текст</h3>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-  </section>
-  <section class="grid w-87.5 grid-cols-2 gap-5 content-center">
-    <h3 class="text-white col-span-2 pl-4">Текст</h3>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-    <article class="w-32 h-32 grid justify-center border border-white rounded py-6 m-auto">
-      <img src="{rec}" alt="#">
-      <p class="text-white text-center pt-2.5">Текст</p>
-    </article>
-  </section>
-</aside>
+<div class="flex overflow-hidden h-screen">
+    <aside class="relative">
+        <section class="relative z-10 bg-main-color grid min-h-screen justify-center content-between gap-10 p-6">
+            <div class="grid gap-10">
+                <a href="#" class="bg-black w-8 h-8 align-middle text-center text-white"><span>{logo}</span></a>
+                <button class="" on:click={toggle}><img src="{templateIcon}" alt="Шаблоны" class="w-8 h-8"></button>
+                <button class=""><img src="{addIcon}" alt="Добавить" class="w-8 h-8"></button>
+                <button class=""><img src="{mediaIcon}" alt="Медия" class="w-8 h-8"></button>
+                <button class=""><img src="{designIcon}" alt="Дизайн" class="w-8 h-8"></button>
+                <button class=""><img src="{adaptiveIcon}" alt="Адаптив" class="w-8 h-8"></button>
+            </div>
+            <div class="grid gap-10">
+                <button><img src="{viewIcon}" alt="Просмотр" class="w-8 h-8"></button>
+                <button class="flex justify-center items-center"><img src="{saveIcon}" alt="Сохранить" class="w-8 h-8"></button>
+            </div>
+        </section>
+        <section id="toolbar" class="grid h-screen border-l-2 p-6 pl-8 gap-6 duration-200 overflow-y-scroll no-scrollbar backdrop-blur-md bg-main-color/90 absolute left-full top-0 transform">
+            <section class="grid w-80 grid-cols-2 gap-5">
+                <h3 class="text-white col-span-2 pl-4">Текст</h3>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Абзац</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Список</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Заголовок</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Цитата</p>
+                </article>
+            </section>
+            <section class="grid w-80 grid-cols-2 gap-5">
+                <h3 class="text-white col-span-2 pl-4">Текст</h3>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Изображение</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Кнопки</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Текст</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Текст</p>
+                </article>
+            </section>
+            <section class="grid w-80 grid-cols-2 gap-5">
+                <h3 class="text-white col-span-2 pl-4">Текст</h3>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Текст</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Текст</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Текст</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Текст</p>
+                </article>
+            </section>
+            <section class="grid w-80 grid-cols-2 gap-5">
+                <h3 class="text-white col-span-2 pl-4">Текст</h3>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Текст</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Текст</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Текст</p>
+                </article>
+                <article class="w-full h-32 grid justify-center border border-white rounded py-6 m-auto">
+                    <img src="{rec}" alt="#" class="m-auto">
+                    <p class="text-white text-center pt-2.5">Текст</p>
+                </article>
+            </section>
+<!--            <button on:click={toggle} class="fixed bg-white right-0 top-1/2 h-11 w-3 rounded-l-md flex justify-center p-0.5"><img src="{leftArrowIcon}" alt="leftArrowIcon" ></button>-->
+        </section>
+    </aside>
+    <main>
+        <img src="{img}" alt="123" class="w-full min-h-screen">
+    </main>
+</div>
