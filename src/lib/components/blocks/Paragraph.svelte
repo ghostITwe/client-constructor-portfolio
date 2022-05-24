0c9114e5feccb4362cd2c8ab82a34731639b9eef
Ishi Inanis <ishi.inanis@gmail.com>
2022-05-24 23:26:57 +0300

    probably fix the rendering issue

--- src/lib/components/blocks/Paragraph.svelte
+++ src/lib/components/blocks/Paragraph.svelte
@@ -10,11 +10,14 @@
     $bar.index = index;
   }
 
+  $: testId = $content.findIndex(el => el.index === index);
+
   $: {
     console.group(index);
     console.log('text', $content[index]?.props.text);
     console.log('props', $content[index]?.props);
     console.log('byindex', $content[index]);
+    console.log('testId', testId);
     console.groupEnd();
   }
 </script>
@@ -22,6 +25,10 @@
 
 <div class="relative">
   {index}
+  <p tabindex="0" on:focus={onfocus}
+     class="bg-blue-100 w-full min-h-[1rem] p-4 rounded-3xl duration-100 cursor-pointer border-2 border-transparent ring-blue-300/30
+        hover:ring-2 hover:border-white
+        focus:border-white focus:backdrop-blur-md focus:bg-blue-100/80 focus:ring-4">{$content[testId]?.props.text ?? 'ERROR'}</p>
   <p tabindex="0" on:focus={onfocus}
      class="bg-blue-100 w-full min-h-[1rem] p-4 rounded-3xl duration-100 cursor-pointer border-2 border-transparent ring-blue-300/30
         hover:ring-2 hover:border-white
