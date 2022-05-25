81eaa5be135d283c708d22854d0d0567d6ed81b7
Ishi Inanis <ishi.inanis@gmail.com>
2022-05-25 20:40:15 +0300

    add icons in AdaptiveBar.svelte

--- src/lib/components/AdaptiveBar.svelte
+++ src/lib/components/AdaptiveBar.svelte
@@ -15,28 +15,28 @@
     </div>
   </article>
   <article class="h-28 flex border border-white rounded cursor-pointer" on:click={() => switchAdaptive('max-w-screen-xl')}>
-    <img class="p-8 border border border-white rounded" src="/assets/images/rec.svg" alt="">
+    <img class="p-8 border border border-white rounded" src="/assets/images/icons/adaptivebar/computer.svg" alt="">
     <div class="m-auto text-center">
       <p class="text-lg">xl</p>
       <p class="font-light">1280px</p>
     </div>
   </article>
   <article class="h-28 flex border border-white rounded cursor-pointer" on:click={() => switchAdaptive('max-w-screen-lg')}>
-    <img class="p-8 border border border-white rounded" src="/assets/images/rec.svg" alt="">
+    <img class="p-8 border border border-white rounded" src="/assets/images/icons/adaptivebar/laptop.svg" alt="">
     <div class="m-auto text-center">
       <p class="text-lg">lg</p>
       <p class="font-light">1024px</p>
     </div>
   </article>
   <article class="h-28 flex border border-white rounded cursor-pointer" on:click={() => switchAdaptive('max-w-screen-md')}>
-    <img class="p-8 border border border-white rounded" src="/assets/images/rec.svg" alt="">
+    <img class="p-8 border border border-white rounded" src="/assets/images/icons/adaptivebar/tablet.svg" alt="">
     <div class="m-auto text-center">
       <p class="text-lg">md</p>
       <p class="font-light">768px</p>
     </div>
   </article>
   <article class="h-28 flex border border-white rounded cursor-pointer" on:click={() => switchAdaptive('max-w-screen-sm')}>
-    <img class="p-8 border border border-white rounded" src="/assets/images/rec.svg" alt="">
+    <img class="p-8 border border border-white rounded" src="/assets/images/icons/adaptivebar/phone.svg" alt="">
     <div class="m-auto text-center">
       <p class="text-lg">sm</p>
       <p class="font-light">640px</p>
--- src/routes/portfolio/index.svelte
+++ src/routes/portfolio/index.svelte
@@ -7,12 +7,13 @@
   import Sidebar from '$lib/components/Sidebar.svelte';
 
   console.log($bar);
+  $: adaptive = $bar?.adaptive;
 </script>
 
 <div class="flex overflow-hidden h-screen">
   <Sidebar/>
   <main class="overflow-y-auto w-full bg-white p-8">
-    <div class={$bar.adaptive ? `${$bar.adaptive} mx-auto` : ''}>
+    <div class={adaptive ?? '' + 'mx-auto duration-200'}>
       {#each $content as { component, id }}
         <svelte:component this={component} {id}/>
       {/each}
