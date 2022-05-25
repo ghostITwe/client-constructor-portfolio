1653c89b9f3edc9254063f3c30b813622cd8c319
Ishi Inanis <ishi.inanis@gmail.com>
2022-05-25 20:44:16 +0300

    fix styles in portfolio

--- src/routes/portfolio/index.svelte
+++ src/routes/portfolio/index.svelte
@@ -6,14 +6,13 @@
   import { bar, content } from '$lib/stores';
   import Sidebar from '$lib/components/Sidebar.svelte';
 
-  console.log($bar);
   $: adaptive = $bar?.adaptive;
 </script>
 
 <div class="flex overflow-hidden h-screen">
   <Sidebar/>
   <main class="overflow-y-auto w-full bg-white p-8">
-    <div class={adaptive ?? '' + 'mx-auto duration-200'}>
+    <div class={(adaptive ?? 'max-w-[1920px]') + ' mx-auto duration-200'}>
       {#each $content as { component, id }}
         <svelte:component this={component} {id}/>
       {/each}
